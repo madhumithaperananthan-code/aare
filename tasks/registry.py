d2e28914701b6141c85964885c6b75da883ecb8a
madhumithaperananthan-code <madhumithaperananthan@gmail.com>
2026-04-12 20:09:22 +0530

    Update registry.py

--- tasks/registry.py
+++ tasks/registry.py
@@ -1,6 +1,11 @@
-from .easy import EasyTask
-from .medium import MediumTask
-from .hard import HardTask
+try:
+    from tasks.easy import EasyTask
+    from tasks.medium import MediumTask
+    from tasks.hard import HardTask
+except ImportError:
+    from easy import EasyTask
+    from medium import MediumTask
+    from hard import HardTask
 
 TASKS = {
     "easy": EasyTask,
